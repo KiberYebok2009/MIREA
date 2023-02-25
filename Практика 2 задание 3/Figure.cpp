0511e02255e3ed8e1b21aae7c33216ed38cd38eb
KiberYebok2009 <126006305+KiberYebok2009@users.noreply.github.com>
2023-02-25 19:59:02 +0300

    Update Figure.cpp

--- Практика 2 задание 3/Figure.cpp
+++ Практика 2 задание 3/Figure.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <locale>
 using namespace std;
-setlocale(LC_ALL,"Russian");
     float figure::S() {
         return(0.5 * abs((x1 - x2) * (y1 + y2) + (x2 - x3) * (y2 + y3) + (x3 - x4) * (y3 + y4) + (x4 - x1) * (y4 + y1)));
     }
